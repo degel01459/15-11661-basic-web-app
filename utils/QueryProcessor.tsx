340cb38f493161714a6ae57b54fd2ae02b9e1f1c
degel01459 <15-11661@usb.ve>
2024-11-18 15:00:33 -0400

    es primo

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,3 +1,13 @@
+function isPrime(num: number): boolean {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+  return true;
+}
+
 export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("shakespeare")) {
     return (
